f9791119f7673d752b69e3b0684fdc9b767121fd
Dzenan Ordagic <dzenan_real_9@live.com>
2024-08-05 10:46:36 +0200

    removed boilerplate from program.cs and launchsettings

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,6 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
+
+//Before build - Add services to the container
+
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//After build - Configure pipeline
 
 app.Run();
