68f16edce8a4773c75b511a05abf5f99f63370eb
Dzenan Ordagic <dzenan_real_9@live.com>
2024-08-10 14:39:01 +0200

    building blocks added with initial create products endpoint

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommandHandler.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS;
+
+//Unit is a void and returns nothing
+public interface ICommandHandler<in TCommand> : ICommandHandler<TCommand, Unit>
+    where TCommand : ICommand<Unit>
+{
+}
+
+public interface ICommandHandler<in TCommand, TResponse> : IRequestHandler<TCommand, TResponse>
+    where TCommand : ICommand<TResponse>
+    where TResponse : notnull
+{
+}
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,10 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 
 //Before build - Add services to the container
-
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssemblies(typeof(Program).Assembly);
+});
 
 var app = builder.Build();
 
 //After build - Configure pipeline
+app.MapCarter();
 
 app.Run();
