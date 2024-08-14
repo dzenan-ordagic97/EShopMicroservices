a5ce32ece97057c870c37b3e670c028e77cc2300
Dzenan <58903620+dzenan-ordagic97@users.noreply.github.com>
2024-08-14 11:31:27 +0200

    Docker support, POST products, GET products GET products/{id}

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdQueryHanler.cs
@@ -0,0 +1,22 @@
+﻿using Catalog.API.Exceptions;
+
+namespace Catalog.API.Products.GetProductById;
+
+public record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdResult>;
+public record GetProductByIdResult(Product Product);
+
+
+internal class GetProductByIdQueryHanler(IDocumentSession documentSession) : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
+{
+    public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+    {
+        var product = await documentSession.LoadAsync<Product>(query.Id, cancellationToken);
+
+        if (product is null)
+        {
+            throw new ProductNotFoundException();
+        }
+
+        return new GetProductByIdResult(product);
+    }
+}
