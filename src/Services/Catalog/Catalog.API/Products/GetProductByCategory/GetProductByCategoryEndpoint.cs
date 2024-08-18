43400fa4867101ac9ab282ff6eb89dd15a34e84d
Dzenan <58903620+dzenan-ordagic97@users.noreply.github.com>
2024-08-18 20:58:23 +0200

    missing commits ?

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryEndpoint.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.API.Products.GetProductByCategory;
+
+public record GetProductByIdRequest(Guid Id);
+public record GetProductByIdResponse(Product Product);
+
+public class GetProductByCategoryEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products/{id}", async ([FromRoute] Guid Id, ISender sender) =>
+        {
+            var result = await sender.Send(new GetProductByIdQuery(Id));
+
+            if (result.Product == null)
+                return Results.NotFound();
+
+            var response = result.Adapt<GetProductByIdResponse>();
+
+            return Results.Ok(response);
+        })
+      .WithName("GetProductsById")
+      .Produces<GetProductByIdResponse>(StatusCodes.Status200OK)
+      .ProducesProblem(StatusCodes.Status400BadRequest)
+      .WithSummary("Get Single Product")
+      .WithDescription("Get Product");
+    }
+}
