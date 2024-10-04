c6c254bf364d05d5447636a52d762efc4794fc40
Dzenan Ordagic <dzenan.ordagic@endava.com>
2024-10-04 20:25:44 +0200

    missing commit

--- src/Services/Ordering/Ordering.API/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -1,9 +1,18 @@
-﻿namespace Ordering.API;
+﻿using BuildingBlocks.Exceptions.Handler;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
+namespace Ordering.API;
 public static class DependencyInjection
 {
     public static IServiceCollection AddApiServices(this IServiceCollection services)
     {
         services.AddCarter();
+
+        services.AddExceptionHandler<CustomExceptionHandler>();
+
+        //TODO: SqlServer missing for some reason
+        //services.AddHealthChecks().AddSqlServer();
 
         return services;
     }
@@ -11,6 +20,14 @@ public static class DependencyInjection
     public static WebApplication UseApiServices(this WebApplication webApplication)
     {
         webApplication.MapCarter();
+
+        webApplication.UseExceptionHandler(options => { });
+
+        webApplication.UseHealthChecks("/health",
+        new HealthCheckOptions
+        {
+            ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+        });
 
         return webApplication;
     }
