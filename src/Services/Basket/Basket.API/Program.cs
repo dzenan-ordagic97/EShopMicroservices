c824cddcbd0b57c1e2dffb7b5fdb5a0f24141b3f
Dzenan <58903620+dzenan-ordagic97@users.noreply.github.com>
2024-10-06 21:39:12 +0200

    rabbitMq integration part 1

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -2,6 +2,7 @@ using Basket.API.Data;
 using BuildingBlocks.Exceptions.Handler;
 using Discount.Grpc;
 using HealthChecks.UI.Client;
+using BuildingBlocks.Messaging.MassTransit;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -46,6 +47,9 @@ builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(
 
     return handler;
 });
+
+//Async Comminucation Services
+builder.Services.AddMessageBroker(builder.Configuration);
 
 
 //Cross cutting services
